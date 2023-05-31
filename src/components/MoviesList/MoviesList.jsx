c381ac3b4c192a809ba9ac2f9b0213fc49bcadcf
ludmila121 <113711608+ludmila121@users.noreply.github.com>
2023-05-31 18:00:49 +0200

    666

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import {PropTypes} from 'prop-types';
 
-
 const MoviesList = ({movies, location}) => (
     <ol>
         {movies.map(movie => {
